31f88a868b544e215119f99011f705cf96b76c9a
stefaniab <stefania13@ru.is>
2015-09-30 13:12:19 +0000

    improvements to getMove

--- src/itml/agents/LearningAgent.java
+++ src/itml/agents/LearningAgent.java
@@ -5,11 +5,13 @@ import itml.simulator.CardDeck;
 import itml.simulator.StateAgent;
 import itml.simulator.StateBattle;
 import weka.classifiers.Classifier;
+import weka.classifiers.functions.MultilayerPerceptron;
 import weka.classifiers.trees.J48;
 import weka.core.Instance;
 import weka.core.Instances;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * User: deong
@@ -24,7 +26,8 @@ public class LearningAgent extends Agent {
 
 	public LearningAgent( CardDeck deck, int msConstruct, int msPerMove, int msLearn ) {
 		super(deck, msConstruct, msPerMove, msLearn);
-		classifier_ = new J48();
+		//classifier_ = new J48();
+		classifier_ = new MultilayerPerceptron();
 	}
 
 	@Override
@@ -89,16 +92,259 @@ public class LearningAgent extends Agent {
 	
 	private Card getMove(StateBattle stateBattle, Card opponentCard)
 	{
-		StateAgent a = stateBattle.getAgentState(0);
-		StateAgent o = stateBattle.getAgentState(1);
+		Random random = new Random();
+		StateAgent a = stateBattle.getAgentState(m_noThisAgent);
+		StateAgent o = stateBattle.getAgentState(m_noOpponentAgent);
+		System.out.println("BATTLESTATE: health/stamina a: " + a.getHealthPoints() + " " + a.getStaminaPoints() + " o: "+ o.getHealthPoints() + " " + o.getStaminaPoints());
+		System.out.println("Location: " + a.getRow() + " " + a.getCol() + " " + o.getRow() + " " + o.getCol());
 		
 		int manhattan = Math.abs(o.getCol() + opponentCard.getCol() - a.getCol()) + Math.abs(o.getRow() + opponentCard.getRow() - a.getRow());
+		int currentManhattan = Math.abs(o.getCol() - a.getCol()) + Math.abs(o.getRow() - a.getRow());
+		System.out.println("manhattan: " + manhattan);
 		
-		if (a.getStaminaPoints() == 0) return new CardRest();
+		// Resting
+		if (a.getStaminaPoints() == 0 || a.getStaminaPoints() < 5 && manhattan > 3) return new CardRest();
+		
+		// Aggressive
+		if (a.getHealthPoints() > o.getHealthPoints())
+		{
+			System.out.println("AGGRESSIVE");
+			// attack if in range
+			if (a.getStaminaPoints() > 1)
+			{
+				if (manhattan < 2) return new CardAttackCardinal();
+				if (manhattan == 2)
+				{
+					if (o.getRow() + opponentCard.getRow() == a.getRow()) return new CardAttackLong(); 
+					if (o.getCol() + opponentCard.getCol() != a.getCol()) return new CardAttackDiagonal();
+				}
+			}
+			// else move closer
+			if (o.getCol() + opponentCard.getCol() == a.getCol()) 
+			{
+				if (o.getRow() + opponentCard.getRow() < a.getRow()) return new CardMoveDown();
+				else return new CardMoveUp();
+			}
+			else if (o.getRow() + opponentCard.getRow() == a.getRow()) 
+			{
+				if (o.getCol() + opponentCard.getCol() < a.getCol()) return new CardMoveLeft();
+				else return new CardMoveRight();
+			}
+			// vertical move
+			if (random.nextBoolean())
+			{
+				if (a.getRow() > o.getRow()) return new CardMoveDown();
+				else return new CardMoveUp();
+			}
+			else
+			{
+				if (a.getCol() > o.getCol()) 
+				{
+					if (a.getCol() > 1 && random.nextBoolean()) return new CardLeapLeft();
+					return new CardMoveLeft();
+				}
+				if (a.getCol() < 3 && random.nextBoolean()) return new CardLeapRight();
+				else return new CardMoveRight();
+			}
+			
+		}
+		// Defensive
+		else if (a.getHealthPoints() < o.getHealthPoints())
+		{
+			System.out.println("DEFENSIVE");
+			if (currentManhattan == 0) 
+			{
+				if (opponentCard.getName() == "cAttackCardinal") 
+				{
+					// leap action
+					if (a.getCol() < 2) return new CardLeapRight();
+					if (a.getCol() > 2) return new CardLeapLeft();
+					if (random.nextBoolean()) return new CardLeapRight();
+					else return new CardLeapLeft();
+				}
+				else if (opponentCard.getName() == "cAttackLong")
+				{
+					// move up/down
+					if (a.getRow() == 0) return new CardMoveUp();
+					if (a.getRow() == 4) return new CardMoveDown();
+					if (random.nextBoolean()) return new CardMoveUp();
+					else return new CardMoveDown();
+				}
+				else if (opponentCard.getName() == "cAttackDiagonal")
+				{
+					// move in any direction
+					return getRandomMove(a);
+				}
+				else if (opponentCard.getName().substring(0,5) == "cMove")
+				{
+					if (a.getStaminaPoints() > 1) return new CardAttackCardinal();
+					else return getRandomMove(a);
+				}
+				else if (opponentCard.getName().substring(0,5) == "cLeap")
+				{
+					if (a.getStaminaPoints() > 1) return new CardAttackLong();
+					else return getRandomMove(a);
+				}
+			}
+			// try to move away
+			int predictedOpponentCol = o.getCol() + opponentCard.getCol();
+			int predictedOpponentRow = o.getRow() + opponentCard.getRow();
+			if (Math.abs(a.getCol() - predictedOpponentCol) > Math.abs(a.getRow() - predictedOpponentRow))
+			{
+				// try to move horizontally
+				if (a.getCol() > predictedOpponentCol)
+				{
+					// try to move right
+					if (a.getCol() == 3) return new CardMoveRight();
+					if (a.getCol() < 3) return new CardLeapRight();
+				}
+				else if (a.getCol() < predictedOpponentCol)
+				{
+					// try to move left
+					if (a.getCol() == 1) return new CardMoveLeft();
+					if (a.getCol() > 1) return new CardLeapLeft();
+				}
+			}
+			else 
+			{
+				// try to move vertically
+				if (a.getRow() > predictedOpponentRow && a.getRow() < 4) return new CardMoveUp();
+				if (a.getRow() < predictedOpponentRow && a.getRow() > 0) return new CardMoveDown();
+			}
+			return getRandomMove(a);
+		}
+		// Equal
+		else
+		{
+			System.out.println("Equal life");
+			if (manhattan > 3) 
+			{
+				// if we're far away, we rest or move closer
+				System.out.println("try to move closer ");
+				if (o.getCol() + opponentCard.getCol() == a.getCol()) 
+				{
+					if (o.getRow() + opponentCard.getRow() < a.getRow()) return new CardMoveDown();
+					else return new CardMoveUp();
+				}
+				else if (o.getRow() + opponentCard.getRow() == a.getRow()) 
+				{
+					if (o.getCol() + opponentCard.getCol() < a.getCol()) return new CardMoveLeft();
+					else return new CardMoveRight();
+				}
+				// vertical move
+				System.out.println("Last option");
+				if (random.nextBoolean())
+				{
+					if (a.getRow() > o.getRow()) return new CardMoveDown();
+					else return new CardMoveUp();
+				}
+				else
+				{
+					if (a.getCol() > o.getCol()) 
+					{
+						if (a.getCol() > 1 && random.nextBoolean()) return new CardLeapLeft();
+						return new CardMoveLeft();
+					}
+					if (a.getCol() < 3 && random.nextBoolean()) return new CardLeapRight();
+					else return new CardMoveRight();
+				}
+			}
+			
+			if (manhattan == 1 && a.getStaminaPoints() > 1) 
+			{
+				if (a.getCol() + opponentCard.getCol() > 0 && a.getCol() + opponentCard.getCol() < 5 && a.getRow() + opponentCard.getRow() > 0 && a.getRow() + opponentCard.getRow() < 5 && random.nextDouble() < 0.2) return opponentCard;
+				return new CardAttackCardinal(); 
+			}
+			if ((Math.abs(a.getCol() - (o.getCol() + opponentCard.getCol())) == 1) && (Math.abs(a.getRow() - (o.getRow() + opponentCard.getRow())) == 1) && a.getStaminaPoints() > 1) 
+			{
+				if (a.getCol() + opponentCard.getCol() > 0 && a.getCol() + opponentCard.getCol() < 5 && a.getRow() + opponentCard.getRow() > 0 && a.getRow() + opponentCard.getRow() < 5 && random.nextDouble() < 0.2) return opponentCard;
+				return new CardAttackDiagonal(); 
+			}
+			if (Math.abs(a.getCol() - (o.getCol() + opponentCard.getCol())) == 2 && a.getStaminaPoints() > 1)
+			{
+				if(random.nextDouble() < 0.5) 
+				{
+					// move out of range
+					double test = random.nextDouble();
+					if (a.getRow() > 0 && test < 0.3) return new CardMoveDown();
+					if (a.getRow() < 4 && test < 0.6) return new CardMoveUp();
+					if ((a.getCol() - (o.getCol() + opponentCard.getCol())) > 0)
+					{
+						if (a.getCol() == 3) return new CardMoveRight();
+						if (a.getCol() < 3)
+						{
+							if (random.nextBoolean()) return new CardMoveRight();
+							else return new CardLeapRight();
+						}
+					}
+				}
+				if (a.getCol() + opponentCard.getCol() > 0 && a.getCol() + opponentCard.getCol() < 5 && a.getRow() + opponentCard.getRow() > 0 && a.getRow() + opponentCard.getRow() < 5 && random.nextDouble() < 0.2) return opponentCard;
+				return new CardAttackLong();
+			}
+			if (manhattan != 0) return getRandomMove(a);
+			if (manhattan == 0)
+			{
+				if (a.getStaminaPoints() > 1)
+				{
+					if (random.nextBoolean()) return new CardAttackLong();
+					else return new CardAttackCardinal();
+				}
+				else 
+				{
+					return getRandomMove(a);
+				}
+			}
+		}
+		
+		
+		System.out.println("No option - old version used");
 		
 		if (manhattan > 3) 
 		{
-			if (a.getStaminaPoints() < 5 ) return new CardRest(); 
+			// if we're far away, we rest or move closer
+			System.out.println("try to move closer ");
+			if (o.getCol() + opponentCard.getCol() == a.getCol()) 
+			{
+				if (o.getRow() + opponentCard.getRow() < a.getRow()) return new CardMoveDown();
+				else return new CardMoveUp();
+			}
+			else if (o.getRow() + opponentCard.getRow() == a.getRow()) 
+			{
+				if (o.getCol() + opponentCard.getCol() < a.getCol()) return new CardMoveLeft();
+				else return new CardMoveRight();
+			}
+			// vertical move
+			System.out.println("Last option");
+			if (random.nextBoolean())
+			{
+				if (a.getRow() > o.getRow()) return new CardMoveDown();
+				else return new CardMoveUp();
+			}
+			else
+			{
+				if (a.getCol() > o.getCol()) 
+				{
+					if (a.getCol() > 1 && random.nextBoolean()) return new CardLeapLeft();
+					return new CardMoveLeft();
+				}
+				if (a.getCol() < 3 && random.nextBoolean()) return new CardLeapRight();
+				else return new CardMoveRight();
+			}
+		}
+		
+		if (o.getHealthPoints() <= a.getHealthPoints() && a.getStaminaPoints() >= 2); 
+		{
+			// attack moves
+			//if (random.nextBoolean()) return getRandomMove(a);
+			System.out.println("try to attack");
+			
+			if (manhattan < 2) return new CardAttackCardinal();
+			if (manhattan == 2)
+			{
+				if (o.getRow() + opponentCard.getRow() == a.getRow()) return new CardAttackLong(); 
+				if (o.getCol() + opponentCard.getCol() != a.getCol()) return new CardAttackDiagonal();
+			}
+			System.out.println("not in range");
 			if (o.getCol() + opponentCard.getCol() == a.getCol()) 
 			{
 				if (o.getRow() + opponentCard.getRow() < a.getRow()) return new CardMoveDown();
@@ -111,18 +357,46 @@ public class LearningAgent extends Agent {
 			}
 		}
 		
-		if (o.getHealthPoints() <= a.getHealthPoints()) 
+		System.out.println("DEFAULT CARD: rest");
+		return new CardRest();
+	}
+	
+	Card getRandomMove(StateAgent a)
+	{
+		Random random = new Random();
+		while(true)
 		{
-			// attack moves
-			if (manhattan < 2) return new CardAttackCardinal();
-			if (manhattan == 2)
+			double card = random.nextDouble();
+			if (card < 1.0 / 6.0)
 			{
-				if (o.getRow() + opponentCard.getRow() == a.getRow()) return new CardAttackLong(); 
-				if (o.getCol() + opponentCard.getCol() != a.getCol()) return new CardAttackDiagonal();
+				// left
+				if (a.getCol() > 0) return new CardMoveLeft();
+			}
+			else if (card < 2.0 / 6.0)
+			{
+				// right
+				if (a.getCol() < 4) return new CardMoveRight();
+			}
+			else if (card < 3.0 / 6.0)
+			{
+				// down
+				if (a.getRow() < 4) return new CardMoveDown();
+			}
+			if (card < 4.0 / 6.0)
+			{
+				// up
+				if (a.getRow() > 0) return new CardMoveUp();
+			}
+			if (card < 5.0 / 6.0)
+			{
+				// leap left
+				if (a.getCol() > 1) return new CardLeapLeft();
+			}
+			else
+			{
+				// leap right
+				if (a.getCol() < 3) return new CardLeapRight();
 			}
 		}
-		
-		
-		return new CardRest();
 	}
 }
